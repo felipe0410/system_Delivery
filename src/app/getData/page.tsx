92b71d447ab4da559da0d31a2cfa9146d3f2e56e
felipe0410 <felipe.montana@whitesoft.com.au>
2025-04-07 10:47:06 -0500

    new label

--- src/app/getData/page.tsx
+++ src/app/getData/page.tsx
@@ -166,7 +166,7 @@ const Page = () => {
 
     try {
       const response = await axios.post(
-        "https://53fb-2803-1a00-153d-c030-2d2f-ceda-df23-4b73.ngrok-free.app/consult",
+        "https://d1a7-191-156-152-48.ngrok-free.app/consult",
         {
           guias: guidesArray,
           password,
